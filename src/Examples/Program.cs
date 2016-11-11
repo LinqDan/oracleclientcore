e631b8dd721da19a9959a9840da3a1f649e08d3b
LinqDan <LinqDan2000@gmail.com>
2016-11-11 01:39:10 -0500

    first commit of unofficial oracle client for .net core
    oracle client for .net core based on mono's oracle client

---
+++ src/Examples/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.Common;
+
+using System.Data.OracleClient;
+
+namespace Examples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Start.");
+
+            Console.WriteLine("create OracleConnection object...");
+            using (var connection = new OracleConnection("Data Source=ORADB12;User ID=SYSTEM;Password=BLISS21"))
+            {
+                Console.WriteLine("Open connection...");
+                connection.Open();
+
+                Console.WriteLine("Create command...");
+                using (var command = connection.CreateCommand())
+                {
+
+                    command.CommandText = "SELECT TABLE_NAME FROM ALL_TABLES ORDER BY 1";
+
+                    Console.WriteLine("Execute reader...");
+                    using (var reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("Tables:");
+
+                        while (reader.Read())
+                        {
+                            string tableName = reader.GetString(reader.GetOrdinal("TABLE_NAME"));
+                            Console.WriteLine(tableName);
+                        };
+                    }
+                }
+            }
+            Console.WriteLine("Done.");
+        }
+
+    }
+}
